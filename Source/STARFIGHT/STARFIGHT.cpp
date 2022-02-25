fca44d4362b0b18d067d51f14133b5ec3e9123c0
Chris <ccarmon3@lion.lmu.edu>
2022-02-24 19:08:54 -0800

    MULTIPLAYER FUNCTIONALITY UPDATES
    - Updates from December

---
+++ Source/STARFIGHT/STARFIGHT.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "STARFIGHT.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, STARFIGHT, "STARFIGHT" );
